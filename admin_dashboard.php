b135424972d4b654fc7e894857ba6eb21a5ca2e6
ddena <isondeanalara@gmail.com>
2025-05-31 19:03:30 +0800

    Admin Dashboard

--- admin_dashboard.php
+++ admin_dashboard.php
@@ -9,7 +9,7 @@
             content="width=device-width, initial-scale=1, shrink-to-fit=no"
         />
 
-        <link rel="stylesheet" href="admin_style.css" />
+        <link rel="stylesheet" href="style.css" />
         <link
             href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
             rel="stylesheet"
